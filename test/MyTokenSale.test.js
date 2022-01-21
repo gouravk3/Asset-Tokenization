9350c298363f24dea9d16ffce29f0fc047151197
Gourav Prasad <GouravP@WinjitTechnologiesPvtLtd.local>
2022-01-21 21:58:02 +0530

    TokenSale Unit test

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -16,4 +16,19 @@ contract("TokenSale Test", async (accounts) => {
         let instance = await Token.deployed();
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
     });
+
+    it("all tokens should be in the TokenSale Smart Contract by default", async () => {
+        let instance = await Token.deployed();
+        let balanceOfTokenSaleSmartContract = await instance.balanceOf(TokenSale.address);
+        let totalSupply = await instance.totalSupply();
+        expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("should be possible to buy tokens", async () => {
+        let tokenInstance = await Token.deployed();
+        let tokenSaleInstance = await TokenSale.deployed();
+        let balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+        expect(tokenSaleInstance.sendTransaction({ from: deployerAccount, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(1)));
+    });
 });
